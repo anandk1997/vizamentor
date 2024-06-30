a3f03ade24d25beef18846e933679bc9cc732733
anandk1997 <anandk01997@gmail.com>
2024-06-30 12:06:31 +0530

    on scroll counter

--- src/components/sections/Satisfied.tsx
+++ src/components/sections/Satisfied.tsx
@@ -1,25 +1,49 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 export const Satisfied = () => {
+  const satisfiedRef = useRef<HTMLDivElement>(null);
+
   const [currentNumber, setCurrentNumber] = useState(500);
+  const [startAnimation, setStartAnimation] = useState(false);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentNumber((prevNumber) => prevNumber + 1);
-    }, 1);
+    const onScroll = () => {
+      if (
+        satisfiedRef.current &&
+        window.scrollY + window.innerHeight >= satisfiedRef.current.offsetTop
+      ) {
+        setStartAnimation(true);
+      }
+    };
 
-    if (currentNumber === 1000) {
-      clearInterval(interval);
+    window.addEventListener("scroll", onScroll);
+    onScroll();
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  useEffect(() => {
+    if (startAnimation) {
+      const interval = setInterval(() => {
+        setCurrentNumber((prevNumber) => prevNumber + 1);
+      }, 1);
+
+      if (currentNumber >= 1000) {
+        clearInterval(interval);
+      }
+
+      return () => clearInterval(interval);
     }
-
-    return () => clearInterval(interval); // Clean up on unmount
-  }, [currentNumber]);
+  }, [startAnimation, currentNumber]);
 
   return (
-    <div className="bg-[#000000] h-52 flex flex-col justify-center align-middle items-center">
-      <div className="flex flex-col justify-center align-middle items-center  px-3 border-x border-[#6D6363]">
+    <div
+      ref={satisfiedRef}
+      className="bg-[#000000] h-52 flex flex-col justify-center align-middle items-center"
+    >
+      <div className="flex flex-col justify-center align-middle items-center px-3 border-x border-[#6D6363]">
         <p className="text-[#F48220] text-8xl font-extrabold">
           {currentNumber}+
         </p>
