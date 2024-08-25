7ec6bba087141146f0225797db1adc3f037a683f
anandk1997 <anandk01997@gmail.com>
2024-08-25 14:04:08 +0530

    login form

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { ChangeEvent, FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import toast from "react-hot-toast";
+import Link from "next/link";
+
+export default function LoginForm() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const router = useRouter();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("/api/signin", formData);
+
+      toast.success("Login successful! Redirecting...");
+
+      setTimeout(() => router.push("/"), 2000);
+    } catch (error: any) {
+      console.error("signup error", error);
+
+      toast.error(
+        error?.response?.data?.message ??
+          "Something went wrong. Please try again."
+      );
+    }
+  };
+
+  return (
+    <div className="max-w-md mx-auto mt-10 p-8 border border-gray-300 rounded-lg shadow-lg">
+      <h1 className="text-2xl font-bold text-center mb-6">Login</h1>
+      <form onSubmit={handleLogin} className="space-y-4">
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >
+          Login
+        </button>
+
+        <Link
+          href={"/sign-up"}
+          className="mt-7 text-center flex m-auto justify-center"
+        >
+          Back to Signup
+        </Link>
+      </form>
+    </div>
+  );
+}
