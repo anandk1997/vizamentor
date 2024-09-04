11865184f264fab32abf45a64cfd963278a772d2
anandk1997 <anandk01997@gmail.com>
2024-09-04 23:53:29 +0530

    users listing api

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,43 @@
+import { dbConnect } from "@/database/database";
+import User from "@/database/model/User";
+import { customErrorResponse } from "@/lib/utils";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await dbConnect();
+
+    const orders = await User.aggregate([
+      {
+        $lookup: {
+          from: "orders",
+          localField: "_id",
+          foreignField: "userId",
+          as: "orderDetails",
+        },
+      },
+
+      {
+        $project: {
+          _id: 0,
+          name: 1,
+          email: 1,
+          phone: 1,
+          address: 1,
+
+          orderDetails: {
+            paymentStatus: 1,
+            productId: 1,
+            amount: 1,
+          },
+        },
+      },
+    ]);
+
+    return NextResponse.json({ data: orders }, { status: 200 });
+  } catch (error) {
+    console.error("error", error);
+
+    return customErrorResponse("Internal Server Error", 500);
+  }
+}
--- src/database/model/Order.ts
+++ src/database/model/Order.ts
@@ -1,12 +1,33 @@
 import mongoose from "mongoose";
 
+const { Schema } = mongoose;
+
 const orderSchema = new mongoose.Schema({
-  orderId: { type: String, required: true },
-  amount: { type: String, required: true },
-  currency: { type: String, required: true },
-  userId: { type: String, required: true },
-  productId: { type: String, required: true },
-  paymentStatus: { type: String, default: "pending" },
+  orderId: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: String,
+    required: true,
+  },
+  currency: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  productId: {
+    type: String,
+    required: true,
+  },
+  paymentStatus: {
+    type: String,
+    default: "pending",
+  },
 });
 
 export default mongoose.models.Order || mongoose.model("Order", orderSchema);
